f27df61f0c6f1e6b0259b7984a8869abcda4871e
Mihai Scripca <icstreispe@yahoo.com>
2021-05-20 17:16:11 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,16 @@
+module "network" {
+  source  = "app.terraform.io/x13-training/network/google"
+  version = "2.5.0"
+  # insert required variables here
+
+  network_name = "x13-network"
+  project_id = var.project  
+
+  subnets = [
+    {
+        subnet_name   = "gaurav-subnet"
+        subnet_ip     = "10.100.10.0/24"
+        subnet_region = var.region
+    }
+  ]
+}
